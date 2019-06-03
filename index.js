e4690b6f3ecb496b0e2ef39704f67dd90a0a3dba
albertfuente <47835134+albertfuente@users.noreply.github.com>
2019-06-03 21:55:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+var title=document.querySelector(".intro");
+
+// var letters="La consultoría transversal"
+// for(let i=0;i<letters.length;i++){
+//   setTimeout(function(){
+//           title.innerHTML+=letters[i];
+//   },20*i)
+//
+//
+//  }
